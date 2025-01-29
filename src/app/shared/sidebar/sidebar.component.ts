9963321b48123e2ea272d88b7b68c400a34521a2
Matheus Colombo da Cruz <matheus.colombo@autoglass.com.br>
2025-01-29 08:58:42 -0300

    Inicia o desenvolvimento do Sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  imports: [
+    FontAwesomeModule
+  ]
 })
 export class SidebarComponent implements OnInit {
+
+  faCoffee = faCoffee;
 
   constructor() { }
 
